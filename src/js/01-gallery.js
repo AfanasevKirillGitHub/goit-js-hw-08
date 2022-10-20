c31c6d24aa39a2ea90d6cefe012c6383d9562e2a
AfanasevKirillGitHub <afanasev.dp@gmail.com>
2022-10-20 22:52:49 +0300

    task-01 commit terminal

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const allGallery = document.querySelector('.gallery');
+const itemMarkup = createNewCardGallery(galleryItems);
+
+function createNewCardGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join('');
+}
+allGallery.insertAdjacentHTML('beforeend', itemMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+// test =====================================================
+lightbox.on('nextDone.simplelightbox', function () {
+  console.log('This is next picture');
+});
+
+lightbox.on('change.simplelightbox', function () {
+  console.log('We wait next picture');
+});
+// ===========================================================
 
 console.log(galleryItems);
