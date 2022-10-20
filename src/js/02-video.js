544890e5e302406e887be7bca2a51dc8510cfd19
AfanasevKirillGitHub <afanasev.dp@gmail.com>
2022-10-21 00:13:48 +0300

    task-02 terminal commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoPlayer = document.querySelector('#vimeo-player');
+const player = new Player(vimeoPlayer);
+const keyStorage = 'videoplayer-current-time';
+const currentTime = localStorage.getItem(keyStorage);
+
+player.on('timeupdate', throttle(setTime, 1000));
+
+function setTime(data) {
+  localStorage.setItem(keyStorage, data.seconds);
+}
+
+player.setCurrentTime(currentTime);
